e3b06a727a807fe88299850a927d9bdb261ecbe9
rinya7 <v.dmitriev0712@gmail.com>
2025-08-20 13:31:28 +0200

    edit some css styles

--- src/layouts/SuperAdminLayout.vue
+++ src/layouts/SuperAdminLayout.vue
@@ -1,84 +1,61 @@
 <!-- SuperAdminLayout (для superadmin) -->
 <!-- src/layouts/SuperAdminLayout.vue -->
 <template>
-  <div>
-    <!-- Шапка для супер-адміна -->
-    <header
-      style="
-        max-width: 1200px;
-        margin: 0 auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        /*padding: 12px;*/
-        padding-top: 24px;
-        padding-left: 12px;
-        padding-right: 12px;
-        border-bottom: 2px solid #2e7d32;
-        position: relative;
-      "
-    >
-      <!-- Навігація -->
-      <nav style="display: flex; gap: 16px">
-        <!--<strong>HoteLotse</strong>-->
-        <RouterLink to="/sa" style="color: #fff; margin-right: 20px"
-          >Dashboard</RouterLink
-        >
-        <RouterLink to="/sa/hotels" style="color: #fff; margin-right: 20px"
-          >Hotels</RouterLink
-        >
-        <RouterLink to="/sa/admins" style="color: #fff">Admins</RouterLink>
-      </nav>
+  <!-- Шапка для супер-адміна -->
+  <header
+    style="
+      max-width: 1200px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      /*padding: 12px;*/
+      padding-top: 24px;
+      padding-left: 12px;
+      padding-right: 12px;
+      border-bottom: 2px solid #2e7d32;
+      position: relative;
+    "
+  >
+    <!-- Навігація -->
+    <nav style="display: flex; gap: 16px">
+      <!--<strong>HoteLotse</strong>-->
+      <RouterLink to="/sa" style="color: #fff; margin-right: 20px"
+        >Dashboard</RouterLink
+      >
+      <RouterLink to="/sa/hotels" style="color: #fff; margin-right: 20px"
+        >Hotels</RouterLink
+      >
+      <RouterLink to="/sa/admins" style="color: #fff">Admins</RouterLink>
+    </nav>
 
-      <!-- Лого -->
-      <div style="display: flex; justify-content: center">
-        <img
-          :src="logo"
-          alt="HoteLotse Logo"
-          style="
-            height: 40px;
-            align-items: center;
-            position: relative;
-            top: 0px;
-          "
-        />
-      </div>
-      <span v-if="auth.isLogged">
-        <em style="margin-right: 10px; color: #fff"
-          >{{ auth.displayName }} ({{ auth.role ?? "?" }})</em
-        >
-        <button @click="onLogout" style="">Logout</button>
-      </span>
-    </header>
-
-    <main
-      style="
-        max-width: 1200px;
-        margin: 0 auto;
-        padding: 24px;
-        padding: 16px;
-        align-items: center;
-        /*position: relative;*/
-      "
-    >
-      <!-- Лого -->
-      <!--<div style="display: flex; justify-content: center; position: relative">-->
-      <!--<img
+    <!-- Лого -->
+    <div style="display: flex; justify-content: center">
+      <img
         :src="logo"
         alt="HoteLotse Logo"
-        style="
-          height: 160px;
-          display: flex;
-          margin: 0 auto;
-          /*align-items: center;*/
-          position: relative;
-          top: -66px; /* смещаем вверх */
-        "
-      />-->
-      <!--</div>-->
-      <RouterView />
-    </main>
-  </div>
+        style="height: 40px; align-items: center; position: relative; top: 0px"
+      />
+    </div>
+    <span v-if="auth.isLogged">
+      <em style="margin-right: 10px; color: #fff"
+        >{{ auth.displayName }} ({{ auth.role ?? "?" }})</em
+      >
+      <button @click="onLogout" style="">Logout</button>
+    </span>
+  </header>
+
+  <main
+    style="
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 24px;
+      padding: 16px;
+      align-items: center;
+    "
+  >
+    <RouterView />
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -98,7 +75,7 @@ function onLogout(): void {
 
 <style scoped>
 .layout-bg {
-  min-height: 100vh;
+  height: full;
   background-image: url("../assets/bg_image.jpg");
   background-size: cover;
   background-position: center;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
-
+// 🔽 підключаєш глобальний CSS
+import "./style.css";
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
