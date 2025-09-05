efe4f978a67df492c420e3a2201a5abc18450757
rinya7 <v.dmitriev0712@gmail.com>
2025-09-05 09:59:53 +0200

    edit some stules such container

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ import { i18n } from "./i18n";
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-
 app.use(i18n);
+
 app.mount("#app");
--- src/views/sa/SaDashboard.vue
+++ src/views/sa/SaDashboard.vue
@@ -1,17 +1,12 @@
 <!-- views/sa/SaDashboard.vue -->
 <template>
-  <div class="p-6 space-y-6">
-    <section
-      class="w-full flex flex-col gap-3 md:flex-row md:items-center md:justify-between"
-    >
-      <h1 class="text-2xl font-semibold">Dashboard</h1>
-    </section>
-    <section class="overflow-hidden rounded-2xl border bg-white">
-      DASHBOARD
-    </section>
-  </div>
+  <section>
+    <h1 class="text-2xl font-semibold">Dashboard</h1>
+    <p class="block">Dashboard</p>
+  </section>
 </template>
 
 <script setup lang="ts">
 // поки без логіки — просто плейсхолдер
 </script>
+<style scoped></style>
